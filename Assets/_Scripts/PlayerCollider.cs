4a407f322dda53e42e2a910d32b72d5a69a1e1d5
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-09-29 17:48:52 -0400

    added scoreboard

--- Assets/_Scripts/PlayerCollider.cs
+++ Assets/_Scripts/PlayerCollider.cs
@@ -1,17 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI; // add this using statement
 
 public class PlayerCollider : MonoBehaviour {
+	// PUBLIC INSTANCE VARIABLES
+	public Text ScoreLabel;
+	public Text LivesLabel;
+
 
 	// PRIVATE INSTANCE VARIABLES
 	private AudioSource[] _audioSources; // array of audio sources
 	private AudioSource _cloudAudioSource, _islandAudioSource;
+
+	private int _score = 0;
+	private int _lives = 5;
 
 	// Use this for initialization
 	void Start () {
 		this._audioSources = this.GetComponents<AudioSource> ();
 		this._cloudAudioSource = this._audioSources [1]; // reference thunder sound
 		this._islandAudioSource = this._audioSources [2]; // reference to the yay sound
+
+		this._ScoreUpdate ();
 	}
 	
 	// Update is called once per frame
@@ -19,13 +29,22 @@ public class PlayerCollider : MonoBehaviour {
 	
 	}
 
+	private void _ScoreUpdate() {
+		this.ScoreLabel.text = "Score: " + this._score;
+		this.LivesLabel.text =  "Lives: " + this._lives;
+	}
+
 	void OnTriggerEnter2D(Collider2D otherGameObject) {
 		if (otherGameObject.tag == "Island") {
 			this._islandAudioSource.Play(); // play the yay sound
+			this._score += 100;
 		}
 
 		if (otherGameObject.tag == "Cloud") {
 			this._cloudAudioSource.Play(); // play the thunder sound
+			this._lives -= 1;
 		}
+
+		this._ScoreUpdate ();
 	}
 }
